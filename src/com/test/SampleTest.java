c3b6388850feda1fcdfef9c39780f539bb955e30
kamaldivyaprabha <kamal458mail@gmail.com>
2022-05-06 00:58:55 +0530

    master

---
+++ src/com/test/SampleTest.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\KAMAL DIVYAPRABHA\\.eclipse\\Sele\\driver\\chromedriver.exe");
+			      WebDriver driver = new ChromeDriver();
+			   
+			      //URL launch
+			      driver.get("https://accounts.google.com/signin");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
